2e15582ab11502781811d0150d3f760e5d888359
Gabriel Torres Martins <gabrieltm06@hotmail.com>
2026-09-13 21:56:09 -0300

    Começo JS

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,8 @@
 const btnGeradorSeNHA = document.querySelector("#novaSenhaGerador");
 const campSenhaGerada = document.querySelector("#senhaResultCamp");
+
+const 
+
+btnGeradorSeNHA.addEventListener("click", () => {
+    console.log("Teste");
+})
