2b5a8612b86aba8dbf3936bc1fd0a5059708c1cb
Gabriel Torres Martins <gabrieltm06@hotmail.com>
2026-09-14 05:38:27 -0300

    Criando geradores de senha

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,24 @@
 const btnGeradorSeNHA = document.querySelector("#novaSenhaGerador");
 const campSenhaGerada = document.querySelector("#senhaResultCamp");
 
-const 
+const geradorLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+const geradorUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+const geradorNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const geradorSymbols = () => {
+    const symbols = "*&¨%$#@!-=+_";
+    return symbols[Match.floor(Math.random() * symbols.lenght)];
+}
+
+console.log(geradorSymbols());
 
 btnGeradorSeNHA.addEventListener("click", () => {
     console.log("Teste");
